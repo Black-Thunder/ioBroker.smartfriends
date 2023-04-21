b8302fb8346e223dc911e33dab28c413ddd6a92d
Black-Thunder <glwars@aol.de>
2023-04-21 14:59:45 +0200

    bumped defaultShcVersion

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ let SchellenbergBridge = null;
 
 // Default gateway parameters
 const defaultPort = 4900;
-const defaultShcVersion = "2.22.3";
+const defaultShcVersion = "3.3.6";
 const defaultShcApiVersion = "2.20";
 const defaultCSymbol = "D19033i";
 
