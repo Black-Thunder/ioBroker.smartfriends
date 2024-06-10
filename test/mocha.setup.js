11128caa864ac644ac8ea7b103d379fa0cac02e5
Black-Thunder <Black-Thunder@users.noreply.github.com>
2024-06-10 08:54:32 +0200

    Update mocha.setup.js

--- test/mocha.setup.js
+++ test/mocha.setup.js
@@ -11,7 +11,7 @@ process.on("unhandledRejection", (e) => {
 // enable the should interface with sinon
 // and load chai-as-promised and sinon-chai by default
 const sinonChai = require("sinon-chai");
-import chaiAsPromised from "chai-as-promised";
+const chaiAsPromised = require("chai-as-promised");
 const { should, use } = require("chai");
 
 should();
