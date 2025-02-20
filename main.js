edfcbedd9bc28b0fbe7b23c2af8df1a71e52d747
Black-Thunder <972581+Black-Thunder@users.noreply.github.com>
2025-02-20 23:21:58 +0100

    updated io-package.json and default values

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ let SchellenbergBridge = null;
 
 // Default gateway parameters
 const defaultPort = 4900;
-const defaultShcVersion = "3.6.0";
+const defaultShcVersion = "3.6.4";
 const defaultShcApiVersion = "3.3";
 const defaultCSymbol = "D19033i";
 
